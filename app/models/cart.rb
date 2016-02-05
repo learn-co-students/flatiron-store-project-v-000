1a623284d1a65d2fcd3e9290dc0dd66a91553504
Joe Burgess <joe@flatironschool.com>
2016-02-05 07:11:38 -0500

    Making add_item save

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,13 +3,13 @@ class Cart < ActiveRecord::Base
   has_many :items, through: :line_items
 
   def add_item(item_id)
-    current_item = self.line_items.find_by(item_id: item_id) 
-    if current_item
-      current_item.quantity += 1
+    line_item = self.line_items.find_by(item_id: item_id) 
+    if line_item 
+      line_item.quantity += 1
+      line_item.save
     else
-      current_item = self.line_items.build(item_id: item_id)
+      self.line_items.create(item_id: item_id)
     end
-    current_item
   end
 
   def total
