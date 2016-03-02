aba848df4dc7b45cb3a05024198b44e92262e42f
github username <mendelblesofsky@gmail.com>
2016-03-02 17:39:34 -0500

    passing most store_feature tests

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,4 +6,10 @@ class StoreController < ApplicationController
     @categories = Category.order(:title)
   end
   
+
+  def index
+  	@categories = Category.all
+  	@items = Item.available_items
+  end
+
 end
