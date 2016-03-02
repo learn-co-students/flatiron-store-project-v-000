283fdb86775b70a677b02b86bf3a48cbe565d7fa
github username <mendelblesofsky@gmail.com>
2016-03-02 17:14:13 -0500

    cart-feature tests passing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,8 @@
 class CategoriesController < ApplicationController
-  before_filter :set_category, only: [:show]
 
-  def index
-    @categories = Category.all
-  end
+	def show
+		@category = Category.find(params[:id])
+		@items = @category.items
+	end
 
-  def show
-  end
-
-  private
-  def set_category
-    @category = Category.find(params[:id])
-  end
 end
