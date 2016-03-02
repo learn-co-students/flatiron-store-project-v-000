c70947089b9045c94caa62a274471f35f50d9af8
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-02 18:36:10 -0500

    Add to cart button working

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,8 @@
 class LineItemsController < ApplicationController
 
+  def create
+    current_user.current_cart.add_item(params[:id])
+    redirect_to store_path
+  end
+
 end
--- spec/features/cart_feature_spec.rb
+++ spec/features/cart_feature_spec.rb
@@ -52,7 +52,7 @@ describe 'Feature Test: Cart', :type => :feature do
        click_button("Checkout")
 
        @user.reload
-       expect(@user.current_cart).to be_nil 
+       expect(@user.current_cart).to be_nil
      end
     end
   end
@@ -140,7 +140,7 @@ describe 'Feature Test: Cart', :type => :feature do
 
       it "Updates quantity when selecting the same item twice" do
         first_item = Item.first
-        2.times do 
+        2.times do
           visit store_path
           within("form[action='#{line_items_path(item_id: first_item)}']") do
             click_button("Add to Cart")
