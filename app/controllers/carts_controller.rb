3af40eedabb665cd549f1c4fcf62eb1a7d41a485
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-02 19:57:30 -0500

    Modify current cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,7 @@
 class CartsController < ApplicationController
 
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,8 @@
 class LineItemsController < ApplicationController
 
   def create
-    current_user.current_cart.add_item(params[:id])
+    new_item = current_user.current_cart.add_item(params[:id])
+    new_item.save
     redirect_to store_path
   end
 
