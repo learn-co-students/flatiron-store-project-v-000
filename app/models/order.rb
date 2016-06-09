ae5278ade0530cda0daf33f56133e8bb507a7fad
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-09 16:57:00 -0400

    Sol - Add #total method to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+
+  def total
+    order_items.inject(0) do |sum, order_item|
+      sum + order_item.total_line_price
+    end
+  end
+
 end
