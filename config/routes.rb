41f19f6a5ae60666c77d1d623b63e6ae4bd38fbc
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 19:14:55 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-  root 'store#index', as: 'store'
-
-  resources :items, only: [:show, :index]
-  resources :categories, only: [:show, :index]
-  resources :users, only: [:show]
-  resources :carts
-  resources :line_items, only: [:create]
-  resources :orders, only: [:show]
-
-  post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
